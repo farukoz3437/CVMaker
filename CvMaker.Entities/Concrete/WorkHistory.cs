82d7a714d39a568c944d15327733ac22dcfef534
farukoz3437 <58840743+farukoz3437@users.noreply.github.com>
2020-02-13 12:05:39 +0300

    13.02.2020-15:05

---
+++ CvMaker.Entities/Concrete/WorkHistory.cs
@@ -0,0 +1,21 @@
+﻿using CvMaker.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CvMaker.Entities.Concrete
+{
+    public class WorkHistory:IEntity
+    {
+        public int WorkHistoryId { get; set; }
+        public string FirmName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Title { get; set; }
+        public string MannerOfWork { get; set; }
+        public string Description { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
